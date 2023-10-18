7a1f19d5204737e4f1b4c7946945b07c081a6b1c
Naitik khoriya <naitikkhoriya144263@gmail.com>
2023-10-18 15:27:58 +0530

    bit

--- chapter_18_nit/bit_manipulation/chapter/chapter/Set_IthBit.java
+++ chapter_18_nit/bit_manipulation/chapter/chapter/Set_IthBit.java
@@ -37,7 +37,31 @@ public class Set_IthBit {
     public static boolean isPowerOrNot(int n){
         return (n &( n-1)) == 0;
     }
+
+    public static int countBit(int n){
+        int count = 0;
+        while (n>0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+
+    public static int fastExpo(int a , int n){
+        int ans = 1;
+        while(n>0){
+            if((n & 1) != 0){
+                ans = ans *a;
+            }
+            a = a *a;
+            n =n>>1;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        System.out.println(isPowerOrNot(15));
+        System.out.println(fastExpo( 3, 5));
+        System.out.println(5^5);
     }
 }
