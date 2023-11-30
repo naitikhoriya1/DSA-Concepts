3fa0338b5ac544dd9fd1b58edb7f9845a9d54782
Naitik khoriya <naitikkhoriya144263@gmail.com>
2023-11-30 13:47:03 +0530

    basic dsa codes

--- fibbonacci.java
+++ fibbonacci.java
@@ -1,7 +1,8 @@
 public class fibbonacci {
     public static int fibb(int n ){
-        if (n == 0) return 0;
-        else if (n==1) return 1;
+        if (n==0 || n==1) {
+            return n;
+        }
         return fibb(n-1) + fibb(n-2);
     }
 
