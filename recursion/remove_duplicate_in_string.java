366a916158bc7088446291567458871439c6f60e
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-04-02 09:32:41 +0530

    better approach

--- recursion/remove_duplicate_in_string.java
+++ recursion/remove_duplicate_in_string.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class remove_duplicate_in_string {
 
     public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
@@ -7,17 +9,20 @@ public class remove_duplicate_in_string {
         }
         char currChar = str.charAt(idx);
         // If not visited then visit the node and print the character.
-        if (map[currChar - 'a'] == true) {
+        if (map[currChar]) {
             removeDuplicates(str, idx + 1, newStr, map);
         } else {
-            map[currChar - 'a'] = true;
+            map[currChar] = true;
             removeDuplicates(str, idx + 1, newStr.append(currChar), map);
         }
 
     }
 
     public static void main(String[] args) {
-        String str = "apnaacollage";
-        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scanner.nextLine();
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[256]);
+        scanner.close(); // Close the scanner
     }
 }
